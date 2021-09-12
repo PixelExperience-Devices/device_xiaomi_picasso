f82bc5080d0219dc843263cf10f91abfe6c7cbf7
baolong24 <1667375308@qq.com>
2021-09-12 10:36:46 +0800

    picasso: Enable VoLTE/VoWiFi for all carrier

--- system_prop.mk
+++ system_prop.mk
@@ -46,5 +46,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RIL
 PRODUCT_PRODUCTY_OVERRIDES += \
-    ro.telephony.default_network=33,33
-
+    ro.telephony.default_network=33,33 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
