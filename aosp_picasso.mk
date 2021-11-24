47f7ceb4a40cac34176c9b38a91343a847bac124
baolong24 <1667375308@qq.com>
2021-11-24 11:04:22 +0800

    picasso: PE-ify

--- aosp_picasso.mk
+++ aosp_picasso.mk
@@ -11,16 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+ TARGET_USES_AOSP_RECOVERY := true
+ TARGET_BOOT_ANIMATION_RES := 1080
+ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_picasso
+PRODUCT_NAME := aosp_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-BUILD_FINGERPRINT := "Redmi/picasso/picasso:11/RKQ1.200826.002/V12.5.2.0.RGICNXM:user/release-keys"
