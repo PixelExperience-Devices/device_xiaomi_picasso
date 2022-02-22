EndCredits/android_device_xiaomi_picasso

picasso: Blacklist FP event
 * After screen unlocked,blacklist FP event to avoid some misbehavior.

Signed-off-by: IsaacChen <tingyi364@gmail.com>

device.mk
@@ -258,6 +258,11 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# IDC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/idc/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc \
+    $(DEVICE_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.default \

PixelExperience-Devices/device_xiaomi_picasso

device.mk
@@ -244,6 +244,11 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# IDC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.default \