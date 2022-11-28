2a43efaee411557cc9c019482ff3e5eaffef1500
LynnrinChan <lynnrin@lynnrin.moe>
2022-11-28 06:47:59 +0000

    picasso: Switch to rro_overlay

--- device.mk
+++ device.mk
@@ -17,13 +17,12 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 $(call inherit-product, vendor/xiaomi/picasso/picasso-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayPicasso \
+    FrameworkResOverlayPicasso \
+    SettingsOverlayPicasso \
+    SystemUIOverlayPicasso \
+    TelephonyOverlayPicasso
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
