fc32633a36b5b53ebefb59de3b153c9282a1cf88
KaguraRinko <lynnrin@lynnrin.moe>
2023-04-16 10:51:46 +0800

    picasso: Use ih8sn for SN

--- picasso_48m/device.mk
+++ picasso_48m/device.mk
@@ -1,5 +1,9 @@
 # Inherit from picasso-common device
 $(call inherit-product, device/xiaomi/picasso/device-common.mk)
 
+# ih8sn
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ih8sn.conf:/system/etc/ih8sn.conf
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/picasso_48m/picasso_48m-vendor.mk)
