9a024f047e440cb29e76415498f9614d73cf76da
Kagura Rinko <lynnrin@lynnrin.moe>
2023-06-11 14:37:03 +0800

    fixup! picasso: Use r416183b to build kernel

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,8 +44,8 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/picasso
 TARGET_KERNEL_CONFIG := vendor/picasso_user_defconfig
-TARGET_KERNEL_CLANG_VERSION := r416183b
 TARGET_KERNEL_LLVM_BINUTILS := false
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/kernel/linux-x86/clang-r416183b
 
 # Platform
 TARGET_BOARD_PLATFORM := lito
